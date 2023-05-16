fixed some rotation issues

test.py
@@ -34,16 +34,17 @@ def make_images():
             add_circle(img, x + 200, y + 200, obj.length, obj.width, (255, 255, 255))
         images.append(img)
 
-def add_past_img(past_img, idx, main_object):
-    img = past_images[main_object.id][idx]
-    past_main_obj = obj_poses[main_object.id][0][idx-1] # this doesn't account for the minus count thing
-    shift_x = main_object.centerX - past_main_obj.centerX
-    shift_y = main_object.centerY - past_main_obj.centerY
-    shift_x = shift_x*math.cos(past_main_obj.rotation) - shift_y*math.sin(past_main_obj.rotation)
-    shift_y = shift_x*math.sin(past_main_obj.rotation) + shift_y*math.cos(past_main_obj.rotation)
-
+def add_past_img(past_img, img, idx, main_object):
+    if idx == 0:
+        return img
+    past_main_obj = obj_poses[main_object.id][0][idx-1]
+    old_shift_x = main_object.centerX - past_main_obj.centerX
+    old_shift_y = main_object.centerY - past_main_obj.centerY
+    shift_x = old_shift_x*math.cos(past_main_obj.rotation) - old_shift_y*math.sin(past_main_obj.rotation)
+    shift_y = old_shift_x*math.sin(past_main_obj.rotation) + old_shift_y*math.cos(past_main_obj.rotation)
+    
     diff_angle = main_object.rotation - past_main_obj.rotation
-
+    # print("rotations", int(math.degrees(past_main_obj.rotation)), int(shift_x), int(shift_y), int( main_object.centerX - past_main_obj.centerX), int( main_object.centerY - past_main_obj.centerY), (math.cos(-past_main_obj.rotation)), (math.sin(-past_main_obj.rotation)))
     # shift then rotate
     # Define the transformation matrix
     M = np.float32([[1, 0, -shift_x], [0, 1, shift_y]])
@@ -64,32 +65,41 @@ def add_past_img(past_img, idx, main_object):
 def add_circle(img, centerX, centerY, length, width, color):
     # Determine the radius of the circle
     radius = max(int(max(length, width) / 2), 1)
-    # if color == (0,255,0):
-        # radius = 10
-        # print("here")
-
-    # Create a white circle image with the same dimensions as the input image
-    # print((centerX), (centerY), (radius), max(length, width) / 2)
     cv2.circle(img, (int(centerX), int(centerY)), radius, color, thickness=-1)
     return img
 
-def frame_to_image_wrt_object(data, idx, count): # egocentric view wrt to object
-    main_object = data.objects[idx]
+def frame_to_image_wrt_object(data, main_object, count): # egocentric view wrt to object
+    # main_object = data.objects[idx]
     main_obj_x = main_object.centerX
     main_obj_y = main_object.centerY
     main_angle = main_object.rotation
     # print("id", main_object.id, main_angle)
 
-    if not os.path.exists("test/"+main_object.id):
-        os.makedirs("test/" + main_object.id)
+    if main_object.classType == "10":
+        # pedestrian, blue
+        obj_class = "pedestrian/"
+    elif main_object.classType == "2":
+        # car, red
+        obj_class = "car/"
+    elif main_object.classType == "3":
+        # van, purple
+        obj_class = "van/"
+    elif main_object.classType == "4":
+        # truck, orange
+        obj_class = "truck/"
+    elif main_object.classType == "5":
+        # bus, yellow
+        obj_class = "bus/"
+    elif main_object.classType == "13":
+        # bicycle, green
+        obj_class = "bicycle/"
+    else:
+        # unknown, black
+        obj_class = "unkown/"
+    if not os.path.exists("test/"+obj_class+main_object.id):
+        os.makedirs("test/" + obj_class + main_object.id)
 
     img = images[count]
-    # img = np.zeros((400, 400, 3), dtype=np.uint8)
-    # for obj in data.objects:
-    #     x = (obj.centerX - main_obj_x)
-    #     y = (obj.centerY - main_obj_y)
-    #     # print ("here", obj.centerX, obj.centerY)
-    #     img = add_circle(img, x + 200, y + 200, obj.length, obj.width, (255, 255, 255))
 
     shift_x = sensorLoc_x - main_obj_x
     shift_y = sensorLoc_y - main_obj_y
@@ -108,11 +118,11 @@ def frame_to_image_wrt_object(data, idx, count): # egocentric view wrt to object
 
     # Apply the rotation to the image
     img = cv2.warpAffine(img, M, (w, h))
-    past_images.setdefault(main_object.id, []).append(img)
+    
+    num, past_img = past_imgs.setdefault(main_object.id, (0, img))
 
-    img = past_images[main_object.id][0]
-    for i in range(1, len(past_images[main_object.id])):
-        img = add_past_img(img, i, main_object)
+    img2 = add_past_img(past_img, img, num, main_object)
+    past_imgs[main_object.id] = (num+1, img2)
 
     # add future pose
     # print("len poses", len(obj_poses[main_object.id][count:]))
@@ -122,8 +132,8 @@ def frame_to_image_wrt_object(data, idx, count): # egocentric view wrt to object
         y = pose.centerY - main_obj_y
         future_pose_img = add_circle(future_pose_img, x + 200, y + 200, pose.length, pose.width, (0, 255, 0))
     future_pose_img = cv2.warpAffine(future_pose_img, M, (w, h))
-    img = cv2.addWeighted(img, 1, future_pose_img, 1, 0)
-    cv2.imwrite("test/{}/{:03d}.png".format(main_object.id, count), img)
+    img = cv2.addWeighted(img2, 1, future_pose_img, 1, 0)
+    cv2.imwrite("test/{}/{}/{:03d}.png".format(obj_class, main_object.id, count), img)
 
 def process_pickle():
     cnt = 0
@@ -138,21 +148,24 @@ def make_gif():
     count = 0
     for ts in data:
 
-        idx = 0
+        # idx = 0
         for obj in ts.objects:
-            frame_to_image_wrt_object(ts, idx, count)
-            idx+=1
+            if len(obj_poses[obj.id][0]) < 10:
+                continue
+            frame_to_image_wrt_object(ts, obj, count)
+            # idx+=1
         count+=1
 
     path = "test"
     c = 0
-    for name in os.listdir(path):
+    for obj_class in os.listdir(path):
         # if c > 1:
         #     break
-        if os.path.isdir(os.path.join(path, name)):
+        # if os.path.isdir(os.path.join(path, name)):
+        for obj_id in os.listdir(os.path.join(path, obj_class)):
             # Do something with the directory
             # Set directory path and image format
-            image_folder = "test/" + name
+            image_folder = "test/" + obj_class + "/" + obj_id
             image_format = 'png'
             print(image_folder)
             
@@ -174,6 +187,7 @@ def make_gif():
 
 obj_poses = {}
 past_images = {}
+past_imgs = {}
 images = []
 
 # Open the pickle file in read binary mode
@@ -181,13 +195,6 @@ with open('array.pickle', 'rb') as f:
     # Load the contents of the file into a Python object
     data = pickle.load(f)
 
-# Use the unpickled data however you need
-# print(type(data[0].objects))
 process_pickle()
 make_images()
-# for i in range(len(images)):
-#     cv2.imwrite("test1/{}.png".format(i), images[i])
-# print(len(images))
-# for key in obj_poses:
-    # print(key, len(obj_poses[key]))
 make_gif()